e70a3524b12d1424a5fd182f7ca7a4abb44dc228
Deme-Coder520 <13576975177@163.com>
2020-11-27 01:50:06 +0800

    commit

--- models/model.go
+++ models/model.go
@@ -14,7 +14,7 @@ type UserInfo struct {
 	Id int
 	Name string `orm:"unique"`
 	Password string
-	Articles []*Article `orm:"rel(m2m)"`
+	Articles []*Article `orm:"reverse(many)"`
 }
 
 // Article 文章信息表
@@ -26,32 +26,24 @@ type Article struct {
 	ArtContent string
 	ArtImg string `orm:"null"`
 	ArtType *ArticleType `orm:"rel(fk)"`//和类型是一对多的关系
-	Users []*UserInfo `orm:"reverse(many)"`//和用户是多对多的关系
+	Users []*UserInfo `orm:"rel(m2m)"`//和用户是多对多的关系
 }
 
 // ArticleType 文章类型表
 type ArticleType struct {
 	Id int
-	TypeName string `orm:"size(48)"`
+	TypeName string `orm:"size(48);unique"`
 	Articles []*Article `orm:"reverse(many)"`
 }
 
 // init 连接数据库
 func init() {
 	// 1.设置数据库基本信息，参数二：驱动的名称"mysql""sqlite""postgres"
-	err := orm.RegisterDataBase("default","mysql","root:root@tcp(127.0.0.1:3306)/db?charset=utf8",30)
-	if err != nil {
-		beego.Info("connect sql fail")
-		return
-	}
+	_=orm.RegisterDataBase("default","mysql","root:root@tcp(127.0.0.1:3306)/db?charset=utf8",30)
 	beego.Info("Connect sql success")
 	// 2.映射model 数据
 	orm.RegisterModel(new(UserInfo),new(Article),new(ArticleType))
 	// 3.生成表,参数一:数据库别名；参数二：是否强制更新表结构（若表结构该表需要切换成true）；参数三：创建过程在终端是否可见
-	err = orm.RunSyncdb("default",false,true)
-	if  err != nil {
-		beego.Info("Create table fail")
-		return
-	}
+	_=orm.RunSyncdb("default",false,true)
 	beego.Info("Create table successful")
 }
